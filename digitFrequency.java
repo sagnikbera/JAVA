4c90a1db639c6e55717875bd102014d065860b72
sagnikbera <sagnikberaofficial@gmail.com>
2024-08-03 03:47:23 +0530

    update

---
+++ digitFrequency.java
@@ -0,0 +1,31 @@
+package JAVA;
+
+/**
+ * https://www.youtube.com/watch?v=U3VZNsMJpD4&list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk&index=77
+ * 
+ * digitFrequency
+ * 
+ * 994543234 => frequency of 4 = 3
+ */
+public class digitFrequency {
+
+    public static void main(String[] args) {
+        
+        int n = 994543234;
+        int dig = 4;
+
+        int count = 0;
+
+        while(n > 0){
+            int rem = n%10;
+            n = n/10;
+
+            if(rem == dig){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+    
+}
