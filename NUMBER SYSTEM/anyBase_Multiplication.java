4f9645869b4df47f64f982c45851e3ad72239e0a
sagnikbera <116964401+sagnikbera@users.noreply.github.com>
2024-08-04 13:40:25 +0530

    Add files via upload

---
+++ NUMBER SYSTEM/anyBase_Multiplication.java
@@ -0,0 +1,11 @@
+/**
+ * https://www.youtube.com/watch?v=_jcwos0_pSg&list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk&index=90
+ * 
+ * anyBase_Multiplication
+ */
+public class anyBase_Multiplication {
+
+    public static void main(String[] args) {
+        
+    }
+}
