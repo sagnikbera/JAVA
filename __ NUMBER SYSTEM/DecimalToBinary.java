22ad784fe69e142c7b835ee0c6340aa3d2b9cd7e
sagnikbera <sagnikberaofficial@gmail.com>
2024-08-20 06:02:00 +0530

    update

---
+++ __ NUMBER SYSTEM/DecimalToBinary.java
@@ -0,0 +1,12 @@
+/**
+ * DecimalToBinary
+ */
+public class DecimalToBinary {
+
+    public static void main(String args[]){
+        int decimal = 3;
+        String binary = Integer.toBinaryString(decimal);
+
+        System.out.println(binary);
+    }
+}
---
+++ __ NUMBER SYSTEM/binaryToDecimal.java
@@ -0,0 +1,14 @@
+/**
+ * binaryToDecimal
+ */
+
+
+public class binaryToDecimal {
+    public static void main(String[] args) {
+        String binary = "10";
+        int decimal = Integer.parseInt(binary, 2);
+
+        System.out.println(decimal);
+    }
+    
+}
