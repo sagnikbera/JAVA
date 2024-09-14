3dbdeeda0849e4778ac4dc761b1e6fbf0af7e256
sagnikbera <sagnikberaofficial@gmail.com>
2024-09-14 23:59:38 +0530

    update

---
+++ Stacks/Stacks.md
@@ -0,0 +1,2 @@
+Video no 103 - Introduction to Stacks
+https://www.youtube.com/watch?v=Fu03UXZgI9s&list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk&index=103&pp=iAQB
---
+++ Stacks/Stock_Span.java
@@ -0,0 +1,13 @@
+//https://www.youtube.com/watch?v=0BsPlzqksZQ&list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk&index=112
+
+// a b c d e f g h i j k 
+//           |         
+//           | |       | 
+//     |   | | | |   | | 
+//   | | | | | | | | | | 
+// | | | | | | | | | | | 
+// | | | | | | | | | | | 
+// 1 2 3 1 2 6 1 1 1 2 3
+
+//using Next greater element on the left (which verient stors index)
+
